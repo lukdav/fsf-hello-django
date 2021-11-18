a75b9b4fc2d3900a5a0a1adb380783f229788d94
lukdav <luke-davies@outlook.com>
2021-11-18 21:16:41 +0000

    Started testing

---
+++ todo/test_forms.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from .forms import ItemForm
+
+class TestItemForm(TestCase):
+
+    def test_item_name_is_required(self):
+        form = ItemForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
