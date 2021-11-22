e87851fa7ece54d218a3d499f418b3c689d500b8
lukdav <luke-davies@outlook.com>
2021-11-22 21:23:06 +0000

    Added test_forms.py and test_views.py

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -8,3 +8,11 @@ class TestItemForm(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn('name', form.errors.keys())
         self.assertEqual(form.errors['name'][0], 'This field is required.')
+
+    def test_done_field_is_not_required(self):
+        form = ItemForm({'name': 'Test Todo Item'})
+        self.assertTrue(form.is_valid())
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = ItemForm()
+        self.assertEqual(form.Meta.fields, ['name', 'done'])
